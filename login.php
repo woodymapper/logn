9ffecf60d46553e7215809cb9816d666dc06d308
M <M@o2.pl>
2022-11-16 13:29:01 +0100

    commit

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class User{
+private int $id;
+private string $login;
+private string $password;
+private string $FirstName;
+private string $Lastname;
+
+
+
+}
+
+public function __construct(string $Login, string $password){
+
+$this->login = $login;
+$this->password = $password;
+
+
+
+}
+
+
+
+
+
+
+?>
