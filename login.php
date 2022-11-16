c494e9bc28fb1689f9e0676a817b24122e4039af
M <M@o2.pl>
2022-11-16 14:26:36 +0100

    modified:   login.php

--- login.php
+++ login.php
@@ -35,7 +35,34 @@ $pq -> execute(); var_dump($pq);
 
 
 }
+public function login(){
+$q = "SELECT * FROM user WHERE login = ?";
+$db = new mysqli('localhost', 'root', '', 'loginform2');
+$pq = $db-> prepare($q);
+$pq -> bind_param('s', $this->login);
+$pq -> execute(); 
+$result = $pq->get_result();
+if(password_verify($this->password, $passwordhash)){
+$this->id = $row['$id'];
+$this->FirstName = $row['FirstName'];
+$this->LastName = $row['LastName'];
+echo"Dobry Login";
 
+}
+else{
+    echo"Zły Login";
+}
+
+if($result->num_rows == 1){
+
+    $row = $result->fetch_assoc();
+
+}
+else{
+    echo"Brak Loginu";
+    return ;
+}
+}
 
 
 ?>
