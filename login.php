f1d8e9698dc7e66019c0004abc41376ae4d056e4
M <M@o2.pl>
2022-11-16 14:50:33 +0100

    modified:   login.php

--- login.php
+++ login.php
@@ -1,68 +1,51 @@
 <?php
+class User {
+    private int $id;
+    private string $login;
+    private string $password;
+    private string $firstName;
+    private string $lastName;
 
+    public function __construct(string $login, string $password) {
+        $this->login = $login;
+        $this->password = $password;
+        $this->firstName = "";
+        $this->lastName = "";
+    }
 
-class User{
-private int $id;
-private string $login;
-private string $password;
-private string $FirstName;
-private string $Lastname;
+    public function register() : bool {
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $db = new mysqli('localhost', 'root', '', 'loginform2');
+        $preparedQuery = $db->prepare($query); 
+        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, 
+                                            $this->firstName, $this->lastName);
+        $result = $preparedQuery->execute();
+        return $result;
+    }
 
-
-
-
-
-    public function __construct(string $Login, string $password){
-
-    $this->login = $login = "a";
-    $this->password = $password;
-        $this->FirstName = "";
-        $this->LastName = "";
-
+    public function login() : bool {
+        $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $db = new mysqli('localhost', 'root', '', 'loginform2');
+        $preparedQuery = $db->prepare($query); 
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $passwordHash = $row['password'];
+            if(password_verify($this->password, $passwordHash)) {
+                $this->id = $row['id'];
+                $this->firstName = $row['firstName'];
+                $this->lastName = $row['lastName'];
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
 }
-
-
-public function register(){
-
-$passwordhash = password_hash($this->password, PASSWORD_ARGON2I);
-$q = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
-$db = new mysqli('localhost', 'root', '', 'loginform2');
-$pq = $db-> prepare($q);
-$pq -> bind_param('ssss', $this->login, $passwordhash, $this->FirstName, $this->LastName);
-$pq -> execute(); var_dump($pq);
-}
-
-public function login(){
-    $q = "SELECT * FROM user WHERE login = ?";
-    $db = new mysqli('localhost', 'root', '', 'loginform2');
-    $pq = $db-> prepare($q);
-    $pq -> bind_param('s', $this->login);
-    $pq -> execute(); 
-    $result = $pq->get_result();
-    if(password_verify($this->password, $passwordhash)){
-    $this->id = $row['$id'];
-    $this->FirstName = $row['FirstName'];
-    $this->LastName = $row['LastName'];
-    echo"Dobry Login";
-    
-    }
-    else{
-        echo"Zły Login";
-    }
-    
-    if($result->num_rows == 1){
-    
-        $row = $result->fetch_assoc();
-    
-    }
-    else{
-        echo"Brak Loginu";
-        return ;
-    }
-    }
-    
-    
-}
-
 ?>
