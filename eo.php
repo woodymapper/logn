25c18a1e329c0266ee6d2f913e74c9db9670a2c5
M <M@o2.pl>
2022-11-23 14:40:09 +0100

    modified:   eo.php 	new file:   templates/register.html.twig

--- eo.php
+++ eo.php
@@ -1,33 +1,25 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rejestracja</title>
-</head>
-<body>
-    <form action="" method="post">
-        <label for="loginID">Login:</label><br>
-        <input type="text" name="login" id="loginID"><br>
-        <label for="passwordID">Hasło:</label><br>
-        <input type="password" name="password" id="passwordID"><br>
-        <label for="firstNameID">Imię:</label><br>
-        <input type="text" name="firstName" id="firstNameID"><br>
-        <label for="lastNameID">Nazwisko:</label><br>
-        <input type="text" name="lastName" id="lastNameID"><br>
-        <input type="submit" value="Rejestruj">
-    </form>
+
 <?php
+require_once('config.php');
 if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
     require_once('login.php');
     $user = new User($_REQUEST['login'], $_REQUEST['password']);
     if($user->register()) {
-        echo "Zarejestrowano poprawnie";
+        $v = array(
+            'message' => "Registracja poprawna:  "
+        );
+        $twig->display('message.html.twig', $v);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     } else {
-        echo "Błąd rejestracji użytkownika";
+        $v = array(
+            'message' => "Błąd "
+        );
+        $twig->display('message.html.twig', $v);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     }
 }
+
+else{
+
+    $twig->display('register.html.twig');
+}
+
 ?>    
-</body>
-</html>
