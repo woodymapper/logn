ae927cf678395662d7a1ac69b8698cb5691f0e4f
M <M@o2.pl>
2022-11-30 13:18:40 +0100

    modified:   index.php 	modified:   templates/login.html.twig

--- index.php
+++ index.php
@@ -10,12 +10,51 @@ Route::add('/', function(){
 Route::add('/login', function(){ 
     //echo "Strona Logowania";
     global $twig;
-    $twig->display("login.html.twig");
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        //jeżeli już podano dane do logowania
+        
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->login()) {
+            //echo "Zalogowano poprawnie użytkownika: ".$user->getName();
+            $v = array(
+                'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
+            );
+            $twig->display('message.html.twig', $v);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        } else {
+            //echo "Błędny login lub hasło";
+            $twig->display('login.html.twig', 
+                                ['message' => "Błędny login lub hasło"]);
+        }
+    } else {
+        //jeśli jeszcze nie podano danych
+        //wyświetl formularz logowania
+        $twig->display('login.html.twig');
+    }
+
+
+
+
+    'post';
 });
 Route::add('/register', function(){ 
    // echo "Strona Rejestracji";
     global $twig;
-    $twig->display("register.html.twig");
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        require_once('login.php');
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->register()) {
+            $v = array(
+                'message' => "Registracja poprawna"
+            );
+            $twig->display('message.html.twig', $v);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        } else {
+            $v = array(
+                'register' => "Błąd "
+            );
+            $twig->display('register.html.twig', $v);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        }
+    }
+
 });
 
 
